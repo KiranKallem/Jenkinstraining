fd73a34b5c18b7103280ba7171f34ba03789b529
prasanna <l.prasanna.rachuri@accenture.com>
2018-10-31 11:25:27 +0530

    done

---
+++ src/main/java/FirstJenkinsJob/SampleAppJenkins/Test3.java
@@ -0,0 +1,21 @@
+package FirstJenkinsJob.SampleAppJenkins;
+
+public class Test3 
+{
+
+	public static void main(String[] args) 
+	{
+		int i=10;
+		if(i>5)
+		{
+			System.out.println("greathen then 5");
+		}
+		else
+		{
+			System.out.println("leassthen then 5");
+		}
+		
+
+	}
+
+}
